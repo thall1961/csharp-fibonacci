9aa52c7d5071b10c4be154c1c7c23d0e1c353b4c
thall1961 <thall1961@gmail.com>
2020-01-16 13:09:33 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace fib
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome! Enter a number or 'x' to quit.\n\n");
+      do
+      {
+        var nums = new List<int> { 0, 1 };
+        Console.WriteLine("\nHow many fibonacci numbers do you want to print?");
+        var response = Console.ReadLine();
+        var flag = ProcessResponse(nums, response, int.TryParse(response, out int n));
+        if (!flag)
+          break;
+      } while (true);
+    }
+
+    static bool ProcessResponse(List<int> nums, string response, bool isInt)
+    {
+      if ((response != "x") && isInt)
+      {
+        // response is an integer and not the escape character
+        var fibNumbers = Int32.Parse(response);
+        Console.WriteLine($"1: {nums[0]}\n2: {nums[1]}");
+        fib(nums, fibNumbers);
+        return true;
+      }
+      else if (!isInt && response != "x")
+      {
+        // response is not a number and not the escape character
+        Console.WriteLine("Please enter a digit.");
+        return true;
+      }
+      else
+      {
+        // response is the escape character, exit program
+        Console.WriteLine("Goodbye!");
+        return false;
+      }
+    }
+
+    static void fib(List<int> nums, int length)
+    {
+      if (nums.Count < length)
+      {
+        nums.Add(nums[nums.Count - 1] + nums[nums.Count - 2]);
+        if (nums.Count > 47)
+        {
+          Console.WriteLine($"{nums.Count}: HUGE NUMBER!!!");
+        }
+        else
+        {
+          Console.WriteLine($"{nums.Count}: {nums[nums.Count - 1]}");
+        }
+        fib(nums, length);
+      }
+    }
+  }
+}
+
---
+++ README.md
@@ -0,0 +1,21 @@
+# Fibonacci Numbers with C
+
+This simple program is to practice using lists. Going off of [this tutorial](https://dotnet.microsoft.com/learn/dotnet/hello-world-tutorial/intro) and going with the challenge at the end found [here on page 51](https://opdhsblobprod02.blob.core.windows.net/contents/c6aea4f5457448ee818b7292ba695982/74d4971ba24d9a4afe01714c2f9a5cec?sv=2015-04-05&sr=b&sig=g8OiDfj140s9XKHJhzPgLg0Ue8Ar0%2F%2BL7Ov6TvHQMb8%3D&st=2020-01-16T17%3A48%3A45Z&se=2020-01-17T17%3A58%3A45Z&sp=r).
+
+The accompanying video tutorial is here (if there's no link, it's coming soon).
+
+### To Run
+
+`git clone https://github.com/thall1961/csharp-fibonacci.git`
+
+`dotnet build`
+
+`dotnet run`
+
+The terminal will prompt you for input.
+
+Have fun!
+
+#### Questions or comments
+
+[here](https://github.com/thall1961/csharp-fibonacci/issues)
